5e6d6860d114142f0e0e3c2b4c5d2f1b88bcf03a
Kokky02 <daniel.kokay@gmail.com>
2023-09-03 16:57:45 +0200

    created JS for date

--- js/date.js
+++ js/date.js
@@ -13,3 +13,24 @@ function formatTime(date) {
 
      return `${hours12.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
 }
+
+/**
+ * 
+ * @param {Date} date 
+ */
+function formatDate(date) {
+     const DAYS = ['neděle', 'pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota']
+     const MONTHS = ['leden', 'únor', 'březen', 'duben', 'květen', 'červen', 'červenec', 'srpen', 'září', 'říjen', 'listopad', 'prosinec']
+
+     return `${DAYS[date.getDay()]}, ${date.getDate()}. ${MONTHS[date.getMonth()]} ${date.getFullYear()}`
+
+     
+
+}
+
+setInterval(() => {
+     const now = new Date();
+
+     timeElement.textContent = formatTime(now);
+     dateElement.textContent = formatDate(now);
+}, 200);
